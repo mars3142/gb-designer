5cf118ea5e0c7202fb3ae19078d07740d5fc7cdf
Peter Siegmund <developer@mars3142.org>
2023-12-07 12:55:14 +0100

    update ui
    Signed-off-by: Peter Siegmund <developer@mars3142.org>

--- lib/ui/components/pixel.dart
+++ lib/ui/components/pixel.dart
@@ -4,21 +4,29 @@ import 'package:gb_designer/theme.dart';
 class GBPixel extends StatelessWidget {
   const GBPixel({
     super.key,
-    required this.index,
+    this.index = 0,
     this.color = colorWhite,
     this.onTap,
+    this.outline = false,
   });
 
   final int index;
   final Color color;
   final Function(int index)? onTap;
+  final bool outline;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => onTap?.call(index),
-      child: ColoredBox(
+    return Container(
+      decoration: BoxDecoration(
         color: color,
+        border: outline ? Border.all(color: colorBlack) : null,
+      ),
+      child: GestureDetector(
+        onTap: () => onTap?.call(index),
+        child: ColoredBox(
+          color: color,
+        ),
       ),
     );
   }
--- lib/ui/designer.dart
+++ lib/ui/designer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gb_designer/theme.dart';
+import 'package:gb_designer/ui/components/pixel.dart';
 import 'package:gb_designer/ui/components/sprite.dart';
 
 class GBDesigner extends StatelessWidget {
@@ -11,13 +13,48 @@ class GBDesigner extends StatelessWidget {
         title: const Text('GB Designer'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8.0),
             child: LayoutBuilder(builder: (context, constraints) {
               return SizedBox(
                 width: constraints.maxWidth / 4,
-                child: const GBSprite(),
+                child: Column(
+                  children: [
+                    const GBSprite(),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text('L'),
+                        SizedBox(height: 12.0, width: 12.0, child: GBPixel(color: colorWhite, outline: true)),
+                        Text('R'),
+                        SizedBox(height: 12.0, width: 12.0, child: GBPixel(color: colorBlack, outline: true)),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Text('Palette'),
+                        const SizedBox(
+                          width: 8.0,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: colorBlack),
+                          ),
+                          child: const Row(
+                            children: [
+                              SizedBox(height: 12.0, width: 12.0, child: GBPixel(color: colorWhite)),
+                              SizedBox(height: 12.0, width: 12.0, child: GBPixel(color: colorLightGray)),
+                              SizedBox(height: 12.0, width: 12.0, child: GBPixel(color: colorGray)),
+                              SizedBox(height: 12.0, width: 12.0, child: GBPixel(color: colorBlack)),
+                            ],
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               );
             }),
           ),
